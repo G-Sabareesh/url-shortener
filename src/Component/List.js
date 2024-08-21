39f11db12b48a7bd5ac178c2b289962468bcdf83
Sabareesh G <sabareeshsoundar@gmail.com>
2024-08-21 12:24:38 +0530

    Add icons and Testing responsive

--- src/Component/List.js
+++ src/Component/List.js
@@ -3,10 +3,7 @@ import ListContent from "./ListContent";
 
 const List = () => {
   return (
-    <div className="d-flex align-items-center justify-content-start col-lg-8 col-md-10 col-12 h-50 p-2 flex-column overflow-auto gap-3">
-        <ListContent />
-        <ListContent />
-        <ListContent />
+    <div className="container d-flex align-items-center justify-content-start col-sm-8 h-50 p-2 flex-column overflow-auto gap-3">
         <ListContent />
     </div>
   );
--- src/Component/ListContent.js
+++ src/Component/ListContent.js
@@ -1,32 +1,42 @@
 import React from "react";
+import { BsQrCode } from "react-icons/bs";
+import { FaRegCopy } from "react-icons/fa6";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const ListContent = () => {
+  const url = "google.comaslkdjf;lsjd;lksjflslkdklf";
+
   return (
     <div
-      className=" col-11 bg-light d-flex justify-content-center align-items-center border  rounded position-relative"
-      style={{ height: "20%" }}
+      className="col-sm-10 d-flex justify-content-center align-items-center  rounded position-relative"
+      style={{ minHeight: "20%" }}
     >
-      <div className="d-flex h-100 col-lg-11 col-md-11 col-12">
-        <div className="d-flex justify-content-center align-items-center  col-6 h-100 bg-primary">
-          Actual URL
-        </div>
-        <div className="d-flex justify-content-center align-items-center text-center col-3 h-100 bg-danger">
-          Shorten URL
-        </div>
-        <div className="d-flex justify-content-evenly align-items-center text-center col-3 h-100 bg-info flex-wrap">
-          <div className="col-6 h-100 bg-warning d-flex justify-content-center align-items-center">
-            QR Code
+      <div
+        className=" d-flex justify-content-center align-items-center bg-light col-sm-11 bordered rounded"
+        style={{ minHeight: "100%" }}
+      >
+        <div className=" d-flex flex-wrap justify-content-center col-9">
+          <div className=" d-flex  justify-content-start align-items-center p-2 col-sm-6 overflow-hidden">
+            <a href={url}>{url}</a>
           </div>
-          <div className="col-6 h-100 bg-white d-flex justify-content-center align-items-center">
-            Copy
+          <div className="d-flex  justify-content-start align-items-center p-2 col-sm-6 overflow-hidden">
+            <a href={url}>{url}</a>
+          </div>
+        </div>
+        <div className="d-flex justify-content-center align-items-center text-center col-sm-3  flex-wrap">
+          <div className="col-sm-5  d-flex justify-content-center align-items-center text-center m-2" title="QR Code" style={{cursor:'pointer'}}>
+            <BsQrCode size={30} />
+          </div>
+          <div className="col-sm-5  d-flex justify-content-center align-items-center text-center m-2" title="Copy" style={{cursor:'pointer'}}>
+            <FaRegCopy size={30} />
           </div>
         </div>
       </div>
       <div
-        className=" close d-flex justify-content-center align-items-center col-lg-1 col-md-1 bg-warning z-3"
-        style={{ cursor: "pointer", height: "100%" }}
+        className=" close d-flex justify-content-center align-items-center ms-1 col-md-1"
+        style={{ cursor: "pointer" }}
       >
-        X
+        <IoMdCloseCircleOutline size={30} />
       </div>
     </div>
   );
